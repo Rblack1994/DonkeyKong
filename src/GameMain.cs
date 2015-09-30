d0364a92a715f0379586482e0cbec5117f7d5207
Rblack1994 <9989692@student.swin.edu.au>
2015-09-30 14:57:33 +1000

    GitHub Repository Initial upload

---
+++ src/GameMain.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Reflection;
+using SwinGameSDK;
+using Color = System.Drawing.Color;
+
+namespace MyGame
+{
+    public class GameMain
+    {
+        public static void Main()
+        {
+            //Start the audio system so sound can be played
+            SwinGame.OpenAudio();
+            
+            //Open the game window
+            SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
+            SwinGame.ShowSwinGameSplashScreen();
+            
+            //Run the game loop
+            while(false == SwinGame.WindowCloseRequested())
+            {
+                //Fetch the next batch of UI interaction
+                SwinGame.ProcessEvents();
+                
+                //Clear the screen and draw the framerate
+                SwinGame.ClearScreen(Color.White);
+                SwinGame.DrawFramerate(0,0);
+                
+                //Draw onto the screen
+                SwinGame.RefreshScreen();
+            }
+            //End the audio
+            SwinGame.CloseAudio();
+            
+            //Close any resources we were using
+            SwinGame.ReleaseAllResources();
+        }
+    }
+}
