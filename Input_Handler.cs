4f9f9373321491d31684853b082fc0a0a7209aad
9983341 <9983341@student.swin.edu.au>
2015-11-01 14:09:18 +1000

    updated tests
    mario position on level reset test

--- Input_Handler.cs
+++ Input_Handler.cs
@@ -352,7 +352,6 @@ namespace MyGame
 				}
 			}
 		}
-			
 	}
 }
 
--- ObjectHandlerTest.cs
+++ ObjectHandlerTest.cs
@@ -8,13 +8,14 @@ namespace MyGame
 	public class ObjectHandlerTest
 	{
 		[Test ()]
-		public void TestCase ()
+		public void TestCaseBarrelCollide ()
 		{
+			// test character dies when hit by barrel
 			StateHandler s = new StateHandler ();
 			ObjectsHandler h = new ObjectsHandler (s);
 
 			// adds a barrel at location 100,119
-			h.AddBarrel (); // addbarrel function required.
+			//h.AddBarrel (); // addbarrel function required.
 			Character c = h.LevelCharacter;
 			c.Xpos = 100;
 			c.Ypos = 119;
@@ -28,11 +29,33 @@ namespace MyGame
 
 		}
 
-		// needs revision
+		[Test ()]
+		public void TestCaseCharPosReset ()
+		{
+			// test that character starts in default position after the level resets
+			StateHandler sh = new StateHandler ();
+			ObjectsHandler oh = new ObjectsHandler (sh);
+			Input_Handler ih = new Input_Handler(sh,oh);
+
+			Character c = oh.LevelCharacter;
+			c.Xpos = 150;
+			c.Ypos = 250;
+
+			//ih.ResetCharacter (); // not written yet - uncomment when added
+
+			// check that is at defualt pos
+			Assert.IsTrue (c.Xpos == 110);
+			Assert.IsTrue (c.Ypos == 552);
+			Assert.IsTrue (sh.Gamestate == GameState.Menu);
+			Assert.IsTrue (sh.Characterstate == CharacterState.Standing);
+
+		}
+		// requires more structured collison coding
+		// need to figure out geometry of sprites for head - where is pixel 1x1?
 		/*[Test ()]
 		public void TestCaseplatformcollision ()
 		{
-
+			// checks collisions with platform
 			StateHandler s = new StateHandler ();
 			ObjectsHandler h = new ObjectsHandler (s);
 			Character c = h.LevelCharacter;
@@ -40,7 +63,7 @@ namespace MyGame
 			c.Statecount = 63;
 			s.ChangeState (CharacterState.Standing);
 			s.Run ();
-			//head
+			//head collision
 
 			int y = 581;
 			c.Xpos = 50;
@@ -49,7 +72,7 @@ namespace MyGame
 			h.LevelCharacterObject ();
 			Assert.IsTrue (c.Ypos == y-1);
 
-			foot - working
+			//foot collision - working
 			c.Statecount = 67;
 			s.ChangeState (CharacterState.Jumping);
 			s.Run ();
